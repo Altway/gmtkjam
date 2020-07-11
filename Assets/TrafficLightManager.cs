8edab62c0e4963aaa523ff6db5ba2996722bbfdf
Remi Gazquez <remi.gazquez@gmail.com>
2020-07-11 17:03:41 +0200

    Added conjugated walker and traffic light

---
+++ Assets/TrafficLightManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum LightColor{Red, Orange, Green}
+
+public class TrafficLightManager : MonoBehaviour
+{
+    public TrafficLight[] trafficRoadDuo = new TrafficLight[2];
+    public WalkerLight[] trafficWalkerDuo = new WalkerLight[2];
+
+    void Start()
+    {
+        trafficRoadDuo[0].conjugatedTrafficLight = trafficRoadDuo[1];
+        trafficRoadDuo[1].conjugatedTrafficLight = trafficRoadDuo[0];
+        trafficRoadDuo[0].conjugatedWalkerLight = trafficWalkerDuo[1];
+        trafficRoadDuo[1].conjugatedWalkerLight = trafficWalkerDuo[0];
+
+        trafficRoadDuo[0].SetColor(LightColor.Red);
+        trafficRoadDuo[1].SetColor(LightColor.Green);
+
+        trafficWalkerDuo[0].conjugatedWalkerLight = trafficWalkerDuo[1];
+        trafficWalkerDuo[1].conjugatedWalkerLight = trafficWalkerDuo[0];
+
+        trafficWalkerDuo[0].SetColor(LightColor.Green);
+        trafficWalkerDuo[1].SetColor(LightColor.Red);
+    }
+
+}
