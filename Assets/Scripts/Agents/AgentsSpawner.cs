e2520c0f2367510fadfc00052e42dc1aba9eb407
raphael-guerault <raphaelguerault@hotmail.fr>
2020-07-11 16:33:36 +0200

    Rage and Calm State - pedestrians

---
+++ Assets/Scripts/Agents/AgentsSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentsSpawner : MonoBehaviour
+{
+    public GameObject agentPrefab;
+    public CityGraph cityGraph;
+    public float cooldown;
+    public float timer;
+    public bool started;
+    void Start()
+    {
+        cityGraph = GameObject.Find("CityGraph").GetComponent<CityGraph>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.W)){
+            started = true;
+        }
+        if(!started){
+            return;
+        }
+        timer += Time.deltaTime;
+        if(timer >= cooldown){
+            timer = 0;
+            SpawnAgent();
+        }
+    }
+    void SpawnAgent(){
+        int tempInt = Random.Range(0, cityGraph.pedestrianEntranceNodes.Count);
+        Walker tempWalk = Instantiate(agentPrefab, cityGraph.pedestrianEntranceNodes[tempInt].transform.position + new Vector3(0,1,0),cityGraph.pedestrianEntranceNodes[tempInt].transform.rotation).GetComponent<Walker>();
+        tempWalk.currentNode = cityGraph.pedestrianEntranceNodes[tempInt];
+        tempWalk.PickDestination();
+    }
+}
--- Assets/Scripts/Graphs/CityNode.cs
+++ Assets/Scripts/Graphs/CityNode.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum NodeType{Crossroad, Street, Pavement, Entrance, Building}
+public enum NodeType{Crossroad, Street, Pavement,PedestrianEntrance,CarEntrance, Building}
 public class CityNode : MonoBehaviour
 {
     public NodeType type;
@@ -42,8 +42,11 @@ public class CityNode : MonoBehaviour
         else if(type == NodeType.Pavement){
             gameObject.GetComponent<Renderer>().material.color = new Color(0.8f,0.8f,0.8f, 1);
         }
-        else if(type == NodeType.Entrance){
+        else if(type == NodeType.PedestrianEntrance){
             gameObject.GetComponent<Renderer>().material.color = new Color(0.2f,0.2f,0.2f, 1);
+        }
+        else if(type == NodeType.CarEntrance){
+            gameObject.GetComponent<Renderer>().material.color = new Color(0.4f,0.4f,0.4f, 1);
         }
         else if(type == NodeType.Building){
             gameObject.GetComponent<Renderer>().material.color = new Color(1f,0f,0f, 1);
