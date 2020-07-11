b9643681401e13c32cb42f9f7a4ca2355735b6e9
raphael-guerault <raphaelguerault@hotmail.fr>
2020-07-11 14:01:46 +0200

    WIP FSM Pedestrian

--- Assets/Scripts/Agents/Walker.cs
+++ Assets/Scripts/Agents/Walker.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Walker : MonoBehaviour
 {
-
     public CityGraph cityGraph;    
+    public AgentsManager agentsManager;
     public CityNode testingDestination;    
     public CityNode currentNode;
-
+    public WalkerState state;
+    public float calmSpeed;
+    public float fearSpeed;
+    public float rageSpeed;
+    public List<CityNode> path = new List<CityNode>();
+    public int index;
     void Start()
     {
         cityGraph = GameObject.Find("CityGraph").GetComponent<CityGraph>();
+        agentsManager = GameObject.Find("GameManager").GetComponent<AgentsManager>();
+        agentsManager.allWalkers.Add(this);
     }
 
-    public Dictionary<CityNode, CityNode> FindPath(CityNode start, CityNode destination) {
+    public List<CityNode> FindPath(CityNode start, CityNode destination) {
         List<CityNode> path = new List<CityNode>();
         Queue<CityNode> frontier = new Queue<CityNode>();
         frontier.Enqueue(start);
@@ -40,14 +47,23 @@ public class Walker : MonoBehaviour
             control++;
 
         }
-
-        return visited;
+        CityNode path_temp = destination;
+        path.Add(destination);
+        while(true) {
+            path_temp = visited[path_temp];
+            path.Add(path_temp);
+            if (visited[path_temp] == currentNode)
+                break;
+        }
+        path.Add(currentNode);
+        path.Reverse();
+        return path;
 
     }
 
-    public void WalkTo(CityNode destination) {
+    /*public void WalkTo(CityNode destination) {
         StartCoroutine(move(destination));
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
@@ -56,25 +72,14 @@ public class Walker : MonoBehaviour
             foreach(CityNode node in cityGraph.allNodes)
                 Debug.Log(node.transform.position);
         }
-        if (Input.GetKeyUp(KeyCode.P)) 
+        /*if (Input.GetKeyUp(KeyCode.P)) 
             WalkTo(testingDestination);
+        */
     }
 
-    IEnumerator move(CityNode destination)
+    /*IEnumerator move(CityNode destination)
     {
         Dictionary<CityNode, CityNode> path_dict = FindPath(currentNode, destination);
-        List<CityNode> path =new List<CityNode>();
-
-        CityNode path_temp = destination;
-        path.Add(destination);
-        while(true) {
-            path_temp = path_dict[path_temp];
-            path.Add(path_temp);
-            if (path_dict[path_temp] == currentNode)
-                break;
-        }
-        path.Add(currentNode);
-        path.Reverse();
 
         /*foreach (KeyValuePair<CityNode, CityNode> kvp in path_dict)
         {
@@ -86,13 +91,26 @@ public class Walker : MonoBehaviour
             if (node != null)
                 print("LIST_NODE: " + node.transform.position);
         }
-        */
+        
 
         foreach(CityNode node in path) {
             gameObject.transform.position = new Vector3(node.transform.position.x, 1, node.transform.position.z);
             currentNode = node;
             yield return new WaitForSeconds(0.1f);
         }
-    }
+    }*/
 
+    public void MoveToNextNode(){
+        if((new Vector3(transform.position.x, 0, transform.position.z) - new Vector3(path[index+1].transform.position.x, 0, path[index+1].transform.position.z)).magnitude < 0.1f){
+            index++;
+            currentNode = path[index];
+            gameObject.transform.position = new Vector3(path[index].transform.position.x, 1, path[index].transform.position.z);
+        }
+        else{
+            gameObject.transform.position += ( new Vector3(path[index+1].transform.position.x, 0, path[index+1].transform.position.z) - new Vector3(transform.position.x, 0, transform.position.z) ).normalized * calmSpeed * Time.deltaTime;
+        }
+    }
+    public void PickDestination(){
+        
+    }
 }
